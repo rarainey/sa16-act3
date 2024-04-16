6090098fd0e21354ca453290ae261b2f47cc27a1
Robert Rainey <r.anthony.rainey@gmail.com>
2024-04-15 22:52:51 -0500

    Added home page, base.html, static css, changed settings for so

--- devportfolio/urls.py
+++ devportfolio/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home, name="home"),
+    path("", RedirectView.as_view(url="home/", permanent=True))
 ]
